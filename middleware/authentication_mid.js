e1531f433b3f760e8dfe046572a2b92779b70cc8
Kamil <xkamilwrobel@gmail.com>
2017-02-20 23:41:08 +0100

    adds authentication by token sent in header

--- middleware/authentication_mid.js
+++ middleware/authentication_mid.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var statusCode = require("../bin/status_code");
+var Token = require('../models/token');
 
 module.exports = function (req, res, next) {
+    var token = req.header('token');
 
     if(req.url !== '/users/login' && req.url!== '/users/register'){
-        if(!req.header('token')){
-            //TODO validate if token exists
+        if(!token){
             res.status(statusCode.HTTP_UNAUTHORIZED).json();
         }else{
-            next();
+            Token.findOne({token: token}, function (err, aToken) {
+                if(err || !aToken) {
+                    next({
+                        statusCode: statusCode.HTTP_UNAUTHORIZED,
+                        message: 'Bad or expired token'
+                    });
+                }else{
+                    next();
+                }
+            });
         }
     }else{
         next();
